2d79639e75e391b9731ea0ef884d3e6d3cba6cd9
michel <mmunhz@gmail.com>
2026-09-09 09:54:54 -0300

    feat(routing): add initial structure

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return <div><h1>MARKETPLACE de NFT</h1></div>
+}
